7ba355abe2a3fa0da6cf1de5d5d4ad69f1b9dc22
biancaraimondi <46532136+biancaraimondi@users.noreply.github.com>
2020-12-02 22:29:04 +0100

    Add files via upload

--- TechWebProject-main/home/primaPagina.js
+++ TechWebProject-main/home/primaPagina.js
@@ -3,48 +3,55 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', '/primaPagina/ottieniStorie', true); //apro connessione tipo GET
 xhr.onreadystatechange = function() {
 	if (xhr.readyState === 4 && xhr.status === 200){
-		var storieJson = xhr.responseText; //ottengo storia in formato json	
-		storie = JSON.parse(storieJson); //ottengo storia in js
+		var storieJson = xhr.responseText; //ottengo storie in formato json
+		storie = JSON.parse(storieJson); //ottengo storie in js
 	}
 };
 xhr.send();
 
-function cambiaPagina(url) {
-		window.location.replace(url);
-}
 
 $(document).ready( function(){
 	var socket = io();
 	var nomeStoria = "";
 	var eta = "";
-	
+	var i = 0;
+
 	$("#eta").click(function() {
 		document.getElementById("autore").hidden = true;
 		document.getElementById("storie").hidden = false;
-		//TODO inserire in eta l'età cliccata nell'html
-		/*var etaList = document.getElementsByTagName("input");
+		
+		//inserisce in eta l'età cliccata nell'html		
+		var etaList = document.getElementsByName("eta");
 		for (i = 0; i < etaList.length; i++) {
 			if (etaList[i].checked) {
-				eta = etaList[i].value;
+				eta = i;
 			}
 		}
-		console.log('PRIMA PAGINA: eta attuale: ' + eta);
-		for (var i=0;i<storie.length;i++){
-			if (eta == storie[i].storie.eta){
-				document.getElementById("nomiStorie").innerHTML += "<div class='form-check'><input name='storia' type='radio' id='" + storie[i].storie.nome + "'><label for='" + storie[i].storie.nome + "'>" + storie[i].storie.nome + "</label></div>";
+		if(eta == 0){
+			eta = "eta1";
+		} else if(eta == 1){
+			eta = "eta2";
+		} else {
+			eta = "eta3";
+		}
+
+		for (var i=0;i<storie.storie.length;i++){
+			if (eta == storie.storie[i].eta){
+				document.getElementById("nomiStorie").innerHTML += "<div class='form-check'><input name='storia' type='radio' id='" + storie.storie[i].nome + "' value='" + storie.storie[i].nome + "'><label for='" + storie.storie[i].nome + "'>" + storie.storie[i].nome + "</label></div>";
 			}
 		}
-		*/
 	});
 	
 	$("#nomiStorie").click(function() {
 		document.getElementById("buttonQRcode").disabled = false;
-		var storiesList = document.getElementsByTagName("input");
+		var storiesList = document.getElementsByName("storia");
+		console.log(storiesList);
 		for (i = 0; i < storiesList.length; i++) {
 			if (storiesList[i].checked) {
 				nomeStoria = storiesList[i].value;
 			}
 		}
+		console.log(nomeStoria);
 		socket.emit('storia', nomeStoria);//es. storiaDaCaricare = 'interrail' e non 'interrail.json'
 	});
 	
